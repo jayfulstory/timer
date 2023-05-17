2bc6b267131bd4c78d5c0b7485c1d250c085787e
Jeahyeon Kim <103633537+jayfulstory@users.noreply.github.com>
2023-05-17 23:28:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+function getTimeSegmentElements(segmentElement) {
+  const segmentDisplay = segmentElement.querySelector('.segment-display');
+  const segmentDisplayTop = segmentDisplay.querySelector(
+    '.segment-display__top'
+  );
+  const segmentDisplayBottom = segmentDisplay.querySelector(
+    '.segment-display__bottom'
+  );
+
+  const segmentOverlay = segmentDisplay.querySelector('.segment-overlay');
+  const segmentOverlayTop = segmentOverlay.querySelector(
+    '.segment-overlay__top'
+  );
+  const segmentOverlayBottom = segmentOverlay.querySelector(
+    '.segment-overlay__bottom'
+  );
+
+  return {
+    segmentDisplayTop,
+    segmentDisplayBottom,
+    segmentOverlay,
+    segmentOverlayTop,
+    segmentOverlayBottom,
+  };
+}
+
+function updateSegmentValues(displayElement, overlayElement, value) {
+  displayElement.textContent = value;
+  overlayElement.textContent = value;
+}
+
+function updateTimeSegment(segmentElement, timeValue) {
+  const segmentElements = getTimeSegmentElements(segmentElement);
+
+  if (
+    parseInt(segmentElements.segmentDisplayTop.textContent, 10) === timeValue
+  ) {
+    return;
+  }
+
+  segmentElements.segmentOverlay.classList.add('flip');
+
+  updateSegmentValues(
+    segmentElements.segmentDisplayTop,
+    segmentElements.segmentOverlayBottom,
+    timeValue
+  );
+
+  function finishAnimation() {
+    segmentElements.segmentOverlay.classList.remove('flip');
+    updateSegmentValues(
+      segmentElements.segmentDisplayBottom,
+      segmentElements.segmentOverlayTop,
+      timeValue
+    );
+
+    this.removeEventListener('animationend', finishAnimation);
+  }
+
+  segmentElements.segmentOverlay.addEventListener(
+    'animationend',
+    finishAnimation
+  );
+}
+
+function updateTimeSection(sectionID, timeValue) {
+  const firstNumber = Math.floor(timeValue / 10) || 0;
+  const secondNumber = timeValue % 10 || 0;
+  const sectionElement = document.getElementById(sectionID);
+  const timeSegments = sectionElement.querySelectorAll('.time-segment');
+
+  updateTimeSegment(timeSegments[0], firstNumber);
+  updateTimeSegment(timeSegments[1], secondNumber);
+}
+const currentTime = Date.now();
+
+function updateAllSegments() {
+  const start = Date.now() - currentTime;
+  const seconds = Math.floor(start / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+
+  updateTimeSection('seconds', seconds % 60);
+  updateTimeSection('minutes', minutes % 60);
+  updateTimeSection('hours', hours);
+  if (hours === 24) {
+    clearInterval(timer);
+    return;
+  }
+}
+
+const timer = setInterval(() => {
+  updateAllSegments();
+}, 1000);
+
+updateAllSegments();
